db8fde487bbc601d8598377437c78a988e49f9e8
Ian <ifoster.98@protonmail.com>
2026-01-11 07:42:26 +1100

    Removing unused auto-generated code from client

--- ToDoClient/src/app/app.config.ts
+++ ToDoClient/src/app/app.config.ts
@@ -4,14 +4,10 @@ import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideDefaultClient } from '../api/providers';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
-    provideDefaultClient({
-      basePath: 'http://localhost:5124'
-    }),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes), provideClientHydration(withEventReplay())
   ]
